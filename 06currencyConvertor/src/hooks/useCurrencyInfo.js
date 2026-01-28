fc8b0482bf66a690483468bf9204fca6a51c7432
Rohan Yadav <yadavrohan98040@gmail.com>
2026-01-29 02:07:20 +0530

    add:created currency convertor app and learn custom hook concept

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { use, useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://api.exchangerate-api.com/v4/latest/USD`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+export default useCurrencyInfo;
